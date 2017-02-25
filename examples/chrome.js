a3e4b8a2c7a31ccbfda3c1af92ea14da7e5a811d
Igari Takeharu <igari.takeharu@gmail.com>
2017-02-25 10:02:44 +0900

    v0.2.7 fixed a problem of publishing and added examples

---
+++ examples/chrome.js
@@ -0,0 +1,12 @@
+const Selenium = require('../lib/selenium-bootstrap.js');
+const selenium = new Selenium({
+  browserName: 'chrome'
+});
+
+selenium.run(function*(driver, webdriver) {
+  yield driver.get('http://www.google.com/ncr');
+  yield driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
+  yield driver.findElement(webdriver.By.name('btnG')).click();
+  yield driver.wait(webdriver.until.titleIs('webdriver - Google Search'), 5000);
+  yield selenium.takeScreenshot();
+});
